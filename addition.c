e5114c9c0c34df782cd3b571f651585d127bde45
abhijit-kakade-dev <abhijitkakade.dev@gmail.com>
2021-04-10 15:23:42 +0530

    Addition

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a, b, sum;
+
+    printf("Enter two numbers to add");
+    scanf("%d%d", &a, &b);
+
+    sum = a + b;
+
+    printf("The sum =%d", sum);
+
+    return 0;
+
+}
