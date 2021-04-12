385915c2ef460bd491bfdf2bfc2255d9bf9880a3
abhijit-kakade-dev <abhijitkakade.dev@gmail.com>
2021-04-13 01:26:53 +0530

    + add spaces between numbers and operators.

--- array/copyarray.c
+++ array/copyarray.c
@@ -8,15 +8,15 @@ int main()
 {
     int i, arr[10], copyarr[10];
 
-    printf("\n Enter 10 elements for array.");
+    printf("\n Enter 10 elements for an array.");
 
-    for(i=0; i<10; i++)
+    for(i = 0; i < 10; i++)
         scanf("%d", &arr[i]);
 
-    for(i=0; i<10; i++)
+    for(i = 0; i < 10; i++)
         copyarr[i] = arr[i];
     
-    for(i=0; i<10; i++)
+    for(i = 0; i < 10; i++)
         printf("\n copyarr[%d] = %d", i, copyarr[i]);
 
     return 0;
